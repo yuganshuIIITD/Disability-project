077e675c3c2556e87510e2b89da25a8cda341e56
Jaskaran320 <write2jaskaransingh@gmail.com>
2022-11-22 12:51:17 +0530

    updated backend and frontend

--- tester.js
+++ tester.js
@@ -3,6 +3,18 @@ const calFunctions = require("./main.js");
 //First run evaluate Website
 // This will output the set of warnings ,notices and errors in the html input
 // And then using its output run evaluateScore
+
+/**
+ * First run evaluateWebsite
+ * Input the url and then 0 , 1 , 2 or 3
+ *  0 - WCAG2A
+ *  1 - WCAG2AA
+ *  2 - WCAG2AAA
+ *  3 - Indian Guidelines
+ * // And then using its output run evaluateScore
+ * //  pass as evaluateScore(output from evaluateWebsite , <Number as give above 0,1,2 or 3>)
+ */
+
 var messageList = [];
 
 var httpList = [
@@ -21,14 +33,14 @@ var httpList = [
 
 //Use promises and async to ensure that code runs in a stepwise manner
 var newPromise = new Promise((resolve, reject) => {
-  resolve((messageList = calFunctions.evaluateWebsite(httpList[1])));
+  resolve((messageList = calFunctions.evaluateWebsite(httpList[2],3)));
 });
 
 newPromise.then((message) => {
   console.log("messageList", message);
-  console.log("messageList Length", message.length);
-  var score = calFunctions.evaluateScore(message);
+  //console.log("messageList Length", message.length);
+  var score = calFunctions.evaluateScore(message,3);
   console.log(score);
-  console.log(calFunctions.toPercent(score, 61));
+  console.log(calFunctions.toPercent(score, 3));
 });
 console.log("executed");

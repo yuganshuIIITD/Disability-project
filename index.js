504e19431f24f7acab9411591d954e25c3de28a3
ashmeet007 <ashmeet19412@iiitd.ac.in>
2023-02-12 00:25:37 +0530

    latest errors fixed

--- index.js
+++ index.js
@@ -24,8 +24,9 @@ var third_score=0;
 var third_per=0;
 var axescore=0;
 var messageList = [];
+
 // const voilations = new Set();
-// var voilations=new Set();
+var voilations=new Set();
 var guidelineTypeToName = {
   0: "A",
   1: "AA",
@@ -33,16 +34,20 @@ var guidelineTypeToName = {
   3: "Indian Guidelines"
 }
 app.post("/",function(req, res){
+    
     var newurl=req.body.url;
     var guidelineType = Object.keys(guidelineTypeToName).find(key => guidelineTypeToName[key] === req.body.guidelineType);
     // console.log(" here ");
-    var newPromise = new Promise((resolve, reject) => {
+    // console.log(guidelineType);
+    messageList = [];
 
-        resolve((messageList = calFunctions.evaluateWebsite(newurl, guidelineType)));
-      });
+    var newPromise = new Promise((resolve, reject) => {
+      resolve(messageList = calFunctions.evaluateWebsite(newurl, guidelineType));
+      // throw new Error("Whoops! URL is wrong");
+    });
       
       newPromise.then((message) => {
-        var voilations=new Set(); // changed
+        voilations=new Set(); // changed
         console.log("messageList", message);
         var score=calFunctions.evaluateScore(message, guidelineType); //changed
         voilations=calFunctions.listofviolations(message, guidelineType);
@@ -67,8 +72,8 @@ app.post("/",function(req, res){
         console.log(score);
         console.log("executed");
 
-      }).catch((message) => {
-        console.error(message);
+      }).catch((error) => {
+        console.log(error);
     });
 
       
@@ -105,7 +110,141 @@ app.get("/guidelines", function(req, res){
 app.get("/submit", function(req, res){
   res.render("home");
 });
-app.listen(process.env.PORT || 5000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("server started");
 });
 //end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express=require("express");
+// const bodyParser=require("body-parser");
+// const app=express();
+// const calFunctions = require("./main.js");
+// // const axe = require("./axe_core.js");
+// const path = require('path');
+// const { render } = require("ejs");
+// const { Console } = require("console");
+// // const Jasmine = require('jasmine');
+// // const jasmine = new Jasmine().jasmine;
+// // jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+// // app.use(express.static('public'));
+// app.set("view engine", "ejs");
+// app.use(express.static(path.join(__dirname, 'css')));
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.get("/",function(req, res){
+//     res.render("home");
+// });
+// // var score=0;
+// // var per=0;
+// var axel_score=0;
+// var axel_per=0;
+// var third_score=0;
+// var third_per=0;
+// var axescore=0;
+// var messageList = [];
+// // const voilations = new Set();
+// // var voilations=new Set();
+// var guidelineTypeToName = {
+//   0: "A",
+//   1: "AA",
+//   2: "AAA",
+//   3: "Indian Guidelines"
+// }
+// app.post("/",function(req, res){
+//     var newurl=req.body.url;
+//     var guidelineType = Object.keys(guidelineTypeToName).find(key => guidelineTypeToName[key] === req.body.guidelineType);
+//     // console.log(" here ");
+//     var newPromise = new Promise((resolve, reject) => {
+
+//         resolve((messageList = calFunctions.evaluateWebsite(newurl, guidelineType)));
+//       });
+      
+//       newPromise.then((message) => {
+//         var voilations=new Set(); // changed
+//         console.log("messageList", message);
+//         var score=calFunctions.evaluateScore(message, guidelineType); //changed
+//         voilations=calFunctions.listofviolations(message, guidelineType);
+//         var per=calFunctions.toPercent(score, guidelineType);  //changed
+//         console.log(voilations);
+//         console.log(score);
+//         console.log(per);
+       
+
+//         // console.log(per);
+//         // axel_per=axe.test_axe(newurl);
+//         console.log(axel_per);
+//         // const {execSync} = require('child_process');
+//         // execSync('sleep 10');
+//         res.render("Score", {
+//             url:newurl,
+//             html_code_sniffer:score,
+//             html_code_sniffer_per:per,
+//             Axel_code:axel_per,
+//             Score3:24
+//         })
+//         console.log(score);
+//         console.log("executed");
+
+//       }).catch((message) => {
+//         console.error(message);
+//     });
+
+      
+// });
+// app.get("/contact", function(req, res){
+//     res.render("contact");
+//   });
+// app.get("/feedback", function(req, res){
+//     res.render("feedback");
+//   });
+// app.get("/resources", function(req, res){
+//   res.render("resources");
+// });
+// app.get("/Analytics", function(req, res){
+//   res.render("analytics",{
+//     html_code_score:per,
+//     html_code_guide_lines:score,
+//     axel_core_score:axel_per,
+//     axel_core_guide_lines:axel_score,
+//     third_score:third_per,
+//     third_guide_lines:third_score
+//   });
+// });
+// app.get("/guidelines", function(req, res){
+//   // console.log("-->");
+//   console.log(
+//     Array.from(voilations.values()) // prints unique Array [1, 2, 3]
+//   )
+//   // console.log(voilations);
+//   res.render("guidelines",{
+//     list:Array.from(voilations.values())
+//   });
+// });
+// app.get("/submit", function(req, res){
+//   res.render("home");
+// });
+// app.listen(process.env.PORT || 5000, function(){
+//     console.log("server started");
+// });
+// //end 
--- main.js
+++ main.js
@@ -76,17 +76,16 @@ var noHtml = false;
 
 async function getHtml(urlInput) {
   var htmlString = "";
+  noHtml=false;
   const browser = await puppeteer.launch();
-  noHtml = false;
+  // const browser = await puppeteer.launch({executablePath: '/usr/bin/google-chrome'});
   try {
-      // const browser = await puppeteer.launch({executablePath: '/usr/bin/google-chrome'});
       const page = await browser.newPage();   
       await page.goto(urlInput, { waitUntil: 'networkidle0' });
       const data = await page.evaluate(() => document.querySelector('*').outerHTML);
       // const data = await page.content();
       // console.log(data);
       htmlString=data;
-      // console.log(htmlString);
       await browser.close();
     } catch (e) {
       console.log("I Encountered an error - URL is not of Type https://example.com ");
@@ -96,8 +95,7 @@ async function getHtml(urlInput) {
       return htmlString;
     }
   // console.log(htmlString);  
-//  console.log("here"); 
- await browser.close();
+  await browser.close();
  return htmlString;
 }
 
@@ -173,12 +171,8 @@ function evaluateScore(messageList, guildelineType) {
     var mp = messageList[i];
     var splitList = mp.toString().split("|");
     var fineSplit = splitList.toString().split(".");
-    // try{
+
     guidelineSet.add(fineSplit[3].substring(0, 5));
-    // }
-    // catch(e){
-    //   console.log(e);
-    // }
     //console.log(fineSplit[3]);
   }
   //Simple Calcaulation logic
@@ -216,4 +210,245 @@ function toPercent(value, guildelineType) {
 }
 
 module.exports = { evaluateWebsite, evaluateScore, toPercent,listofviolations};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /**
+//  * First run evaluateWebsite
+//  * Input the url and then 0 , 1 , 2 or 3
+//  *  0 - WCAG2A
+//  *  1 - WCAG2AA
+//  *  2 - WCAG2AAA
+//  *  3 - Indian Guidelines
+//  * // And then using its output run evaluateScore
+//  * //  pass as evaluateScore(output from evaluateWebsite , <Number as give above 0,1,2 or 3>)
+//  */
+// var jsdom = require("jsdom");
+// var { JSDOM } = jsdom;
+// var fs = require("fs");
+// const puppeteer = require('puppeteer'); 
+
+// //Indian Guidelines list
+// const indianGuidelines = [
+//   "1_1_1",
+//   "1_2_1",
+//   "1_2_2",
+//   "1_2_3",
+//   "1_2_5",
+//   "1_3_1",
+//   "1_3_2",
+//   "1_3_3",
+//   "1_4_1",
+//   "1_4_2",
+//   "1_4_3",
+//   "1_4_4",
+//   "1_4_5",
+//   "2_1_1",
+//   "2_1_2",
+//   "2_2_1",
+//   "2_2_2",
+//   "2_3_2",
+//   "2_4_1",
+//   "2_4_2",
+//   "2_4_3",
+//   "2_4_4",
+//   "2_4_5",
+//   "2_4_6",
+//   "2_4_7",
+//   "3_1_1",
+//   "3_1_2",
+//   "3_2_1",
+//   "3_2_2",
+//   "3_2_3",
+//   "3_2_4",
+//   "3_3_1",
+//   "3_3_2",
+//   "3_3_3",
+//   "3_3_4",
+//   "4_1_1",
+//   "4_1_2",
+// ];
+
+// //WCAG2A
+// const wcag2aSize = 30;
+// //WCAG2AA
+// const wcag2aaSize = wcag2aSize + 20;
+// //WCAG2AAA
+// const wcag2aaaSize = wcag2aaSize + 28;
+// //Indian AAA
+// const indianGuidelinesSize = 37;
+// //Array for the below nums
+// const guidelineCount = [
+//   wcag2aSize,
+//   wcag2aaSize,
+//   wcag2aaaSize,
+//   indianGuidelinesSize,
+// ];
+
+// var HTMLCS = fs.readFileSync("./build/HTMLCS.js", "utf-8");
+// var vConsole = new jsdom.VirtualConsole();
+// var noHtml = false;
+
+// async function getHtml(urlInput) {
+//   var htmlString = "";
+//   const browser = await puppeteer.launch();
+//   noHtml = false;
+//   try {
+//       // const browser = await puppeteer.launch({executablePath: '/usr/bin/google-chrome'});
+//       const page = await browser.newPage();   
+//       await page.goto(urlInput, { waitUntil: 'networkidle0' });
+//       const data = await page.evaluate(() => document.querySelector('*').outerHTML);
+//       // const data = await page.content();
+//       // console.log(data);
+//       htmlString=data;
+//       // console.log(htmlString);
+//       await browser.close();
+//     } catch (e) {
+//       console.log("I Encountered an error - URL is not of Type https://example.com ");
+//       // console.log(e);
+//       noHtml=true;
+//       await browser.close();
+//       return htmlString;
+//     }
+//   // console.log(htmlString);  
+// //  console.log("here"); 
+//  await browser.close();
+//  return htmlString;
+// }
+
+// async function evaluateWebsite(websiteHttp, guildelineType) {
+//   //document = Jsoup.connect(websiteHttp).get();
+//   //var websiteHtml = new XMLSerializer().serializeToString(doc)
+//   var messageList = [];
+//   // Forward messages to the console.
+//   vConsole.on("log", function (message) {
+//     if (message != "done") {
+//       if (guildelineType != 3) {
+//         messageList.push(message);
+//       } else if (isIndianGuidlines(message)) {
+//         messageList.push(message);
+//       }
+//     }
+//   });
+//   var urlInput = "";
+
+//   urlInput = await getHtml(websiteHttp);
+//   if (noHtml == true) {
+//     return [];
+//   }
+ 
+//   var dom = new JSDOM(urlInput, {
+//     runScripts: "dangerously",
+//     virtualConsole: vConsole,
+//   });
+
+//   //Running the evaluation of website
+//   dom.window.eval(HTMLCS);
+
+//   if (guildelineType == 0) {
+//     dom.window.HTMLCS_RUNNER.run("WCAG2A");
+//   } else if (guildelineType == 1) {
+//     dom.window.HTMLCS_RUNNER.run("WCAG2AA");
+//   } else if (guildelineType == 2) {
+//     dom.window.HTMLCS_RUNNER.run("WCAG2AAA");
+//   } else if (guildelineType == 3) {
+//     dom.window.HTMLCS_RUNNER.run("WCAG2AAA");
+//   } else {
+//     console.log("Incorrect guildelineType");
+//   }
+//   return messageList;
+// }
+
+// function isIndianGuidlines(message) {
+//   var splitList = message.toString().split("|");
+//   var fineSplit = splitList.toString().split(".");
+//   var finalM = fineSplit[3].substring(0, 5);
+//   //console.log("finallM",finalM);
+//   if (indianGuidelines.includes(finalM)) {
+//     //console.log("true")
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// function evaluateScore(messageList, guildelineType) {
+//   if (messageList.length == 0) {
+//     console.log("Empty Message List: Assigning 0 score");
+//     return 0;
+//   }
+
+//   const len = messageList.length;
+
+//   const guidelineSet = new Set();
+
+//   //Creating a set for guidelines not followed.
+//   for (let i = 0; i < len; i++) {
+//     //console.log(messageList[i]);
+//     var mp = messageList[i];
+//     var splitList = mp.toString().split("|");
+//     var fineSplit = splitList.toString().split(".");
+//     // try{
+//     guidelineSet.add(fineSplit[3].substring(0, 5));
+//     // }
+//     // catch(e){
+//     //   console.log(e);
+//     // }
+//     //console.log(fineSplit[3]);
+//   }
+//   //Simple Calcaulation logic
+//   var score = guidelineCount[guildelineType] - guidelineSet.size;
+//   // console.log(guidelineSet);
+//   return score;
+// }
+
+// function listofviolations(messageList, guidelineType ) {
+
+//   const len = messageList.length;
+//   const guidelineSet = new Set();
+//   //Creating a set for guidelines not followed.
+//   for (let i = 0; i < len; i++) {
+//     //console.log(messageList[i]);
+//     var mp = messageList[i];
+//     var splitList = mp.toString().split("|");
+//     var fineSplit = splitList.toString().split(".");
+//     guidelineSet.add(fineSplit[3].substring(0, 5));
+//     //console.log(fineSplit[3]);
+//   }
+//   return guidelineSet;
+// }
+
+// function toPercent(value, guildelineType) {
+//   var returnValue = 0.0;
+//   try {
+//     returnValue = ((value / guidelineCount[guildelineType]) * 100).toFixed(2);
+//   } catch (e) {
+//     console.error("toPercent error:",e);
+//     return returnValue;
+//   } finally {
+//     return returnValue;
+//   }
+// }
+
+// module.exports = { evaluateWebsite, evaluateScore, toPercent,listofviolations};
 
